1c38ee282b53cf7b4f972974f9230f232d86466f
Enric <enricorillosa@hotmail.es>
2022-10-10 17:21:41 +0200

    first commit
    exercise done

---
+++ lab-vue-setup/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="helloWorld">Greet</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greet: 'Hello World!'
+    }
+  },
+  methods: {
+    helloWorld() {
+      alert(this.greet)
+      console.log(this.greet)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
